750a633b180272bdf158ef81949b1e7ca14b808e
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2023-11-30 09:31:05 +0100

    Fix test script (again)

--- test/svelte/index.ts
+++ test/svelte/index.ts
@@ -1,60 +1,55 @@
-import { Component, define, HTML, html } from '@chialab/dna';
+import { Component, customElement, HTML, html } from '@chialab/dna';
 
-export const TestElement = define(
-    'test-element',
-    class TestElement extends Component {
-        static get properties() {
-            return {
-                stringProp: String,
-                booleanProp: Boolean,
-                numericProp: Number,
-                objectProp: Object,
-            };
-        }
-
-        declare stringProp?: string;
-        declare booleanProp?: boolean;
-        declare numericProp?: number;
-        declare objectProp?: object;
-
-        render() {
-            return html`
-                <span class="text">${this.stringProp}</span>
-                ${this.booleanProp ? html`<span class="boolean">true</span>` : ''}
-                <span class="number">${this.numericProp}</span>
-                <span class="object">${JSON.stringify(this.objectProp)}</span>
-            `;
-        }
+@customElement('test-element')
+export class TestElement extends Component {
+    static get properties() {
+        return {
+            stringProp: String,
+            booleanProp: Boolean,
+            numericProp: Number,
+            objectProp: Object,
+        };
     }
-);
 
-export const TestLink = define(
-    'test-link',
-    class TestLink extends HTML.Anchor {
-        static get properties() {
-            return {
-                stringProp: String,
-                booleanProp: Boolean,
-                numericProp: Number,
-                objectProp: Object,
-            };
-        }
+    declare stringProp?: string;
+    declare booleanProp?: boolean;
+    declare numericProp?: number;
+    declare objectProp?: object;
 
-        declare stringProp?: string;
-        declare booleanProp?: boolean;
-        declare numericProp?: number;
-        declare objectProp?: object;
-
-        render() {
-            return html`
-                <span class="text">${this.stringProp}</span>
-                ${this.booleanProp ? html`<span class="boolean">true</span>` : ''}
-                <span class="number">${this.numericProp}</span>
-                <span class="object">${JSON.stringify(this.objectProp)}</span>
-            `;
-        }
-    },
-    {
-        extends: 'a',
+    render() {
+        return html`
+            <span class="text">${this.stringProp}</span>
+            ${this.booleanProp ? html`<span class="boolean">true</span>` : ''}
+            <span class="number">${this.numericProp}</span>
+            <span class="object">${JSON.stringify(this.objectProp)}</span>
+        `;
     }
-);
+}
+
+@customElement('test-link', {
+    extends: 'a',
+})
+export class TestLink extends HTML.Anchor {
+    static get properties() {
+        return {
+            stringProp: String,
+            booleanProp: Boolean,
+            numericProp: Number,
+            objectProp: Object,
+        };
+    }
+
+    declare stringProp?: string;
+    declare booleanProp?: boolean;
+    declare numericProp?: number;
+    declare objectProp?: object;
+
+    render() {
+        return html`
+            <span class="text">${this.stringProp}</span>
+            ${this.booleanProp ? html`<span class="boolean">true</span>` : ''}
+            <span class="number">${this.numericProp}</span>
+            <span class="object">${JSON.stringify(this.objectProp)}</span>
+        `;
+    }
+}
