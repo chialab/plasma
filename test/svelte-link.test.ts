a96f7b97a061ca7a5ce37e02db4e3f7b9e286d2f
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2024-07-04 08:59:37 +0200

    Add support for Svelte 5

--- test/svelte-link.test.ts
+++ test/svelte-link.test.ts
@@ -1,53 +1,38 @@
-import { tick } from 'svelte';
-import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+import { render } from '@testing-library/svelte';
+import type { SvelteComponent } from 'svelte';
+import { describe, expect, test, vi } from 'vitest';
 import { TestLink } from './src/svelte/TestLink';
 import type { TestLink as HTMLTestLink } from './src/TestLink';
 
-let host: HTMLElement;
-
 describe('Link', () => {
-    beforeEach(() => {
-        host = document.createElement('div');
-        document.body.appendChild(host);
-    });
-
-    afterEach(() => {
-        host.remove();
-    });
-
     test('mount component', async () => {
-        const instance = new TestLink({
-            target: host,
-            props: {
-                'stringProp': 'test',
-                'booleanProp': true,
-                'numericProp': 1,
-                'objectProp': { test: true },
-                'data-attr': 'test',
-            },
+        const onClick = vi.fn((event) => event.preventDefault());
+        const onStringChange = vi.fn();
+        const { rerender, container } = render(TestLink as unknown as new (...args: any[]) => SvelteComponent, {
+            'stringProp': 'test',
+            'booleanProp': true,
+            'numericProp': 1,
+            'objectProp': { test: true },
+            'data-attr': 'test',
         });
-        const node = host.querySelector('a') as HTMLTestLink;
+        const node = container.querySelector('a') as HTMLTestLink;
         expect(node.stringProp).toBe('test');
         expect(node.booleanProp).toBe(true);
         expect(node.numericProp).toBe(1);
         expect(node.objectProp).toEqual({ test: true });
         expect(node.defaultValue).toBe(0);
         expect(node.getAttribute('data-attr')).toBe('test');
-        const onStringChange = vi.fn();
-        instance.$on('stringchange', onStringChange);
-        const onClick = vi.fn((event) => event.preventDefault());
-        instance.$on('click', onClick);
-        expect(instance).toBeTruthy();
-        expect(host.innerHTML).toMatchSnapshot();
+        expect(container.innerHTML).toMatchSnapshot();
         expect(onStringChange).not.toHaveBeenCalled();
         expect(onClick).not.toHaveBeenCalled();
-        instance.$$set({
+        await rerender({
+            onclick: onClick,
+            onstringchange: onStringChange,
             stringProp: 'changed',
         });
-        await tick();
-        expect(host.innerHTML).toMatchSnapshot();
-        expect(onStringChange).toHaveBeenCalled();
+        expect(container.innerHTML).toMatchSnapshot();
+        expect(onStringChange).toHaveBeenCalledOnce();
         node.click();
-        expect(onClick).toHaveBeenCalled();
+        expect(onClick).toHaveBeenCalledOnce();
     });
 });
